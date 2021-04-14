d3b9a87357475a0f8bbc5ba22e308d0d0ca2bbef
irrem <50523883+irrem@users.noreply.github.com>
2021-04-15 00:42:03 +0300

    home page has been edited

--- src/components/Anasayfa.js
+++ src/components/Anasayfa.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Button } from 'bootstrap';
+import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 const UyeOl = () => {
   return (
@@ -11,15 +12,105 @@ const UyeOl = () => {
             <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Film Başlık</a>
           </center>
           <hr />
+          <div className="butonContainer">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> Bilet Al</a>
+            </div>          
+        </Col>
+        <Col md='6'> 
+        <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Film Başlık</a><br />
           <p>
             Bir intihara tanık olduktan sonra buna neden olanları bulmak isteyen alçakgönüllü bir
             polis memuru, reddedilmiş iki suç dosyasını araştırmaya karar verir. Bu araştırması onu
             korkunç bir sır saklayan bir çifte yönlendirir.
-          </p>
+          </p><br /> 
+        <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Seans Saatleri</a><br />
+          <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 10:30</a>
+            </div> 
+             <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 14:30</a>
+            </div> 
+             <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 17:30</a>
+            </div> 
+          </Col>
+        <Col md='3'>
+          <div className="butonContainer-left">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> Sinema </a>
+            </div>
+            <div className="butonContainer-left">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> Tiyatro </a>
+            </div> 
+            <div className="butonContainer-left">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> TalkShow</a>
+            </div> 
+         </Col>
+      </Row><hr/>
+      <Row>
+        <Col md='3'>
+          <center>
+            <img src='https://img01.imgsinemalar.com/images/afis_buyuk/g/good-morning-veronica-1614678038.jpg' />
+            <br />
+            <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Film Başlık</a>
+          </center>
+          <hr />
+          <div className="butonContainer">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> Bilet Al</a>
+            </div>          
         </Col>
-        <Col md='6'>6 Parça</Col>
-        <Col md='3'>3 Parça</Col>
-      </Row>
+        <Col md='6'> 
+        <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Film Başlık</a><br />
+          <p>
+            Bir intihara tanık olduktan sonra buna neden olanları bulmak isteyen alçakgönüllü bir
+            polis memuru, reddedilmiş iki suç dosyasını araştırmaya karar verir. Bu araştırması onu
+            korkunç bir sır saklayan bir çifte yönlendirir.
+          </p><br /> 
+        <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Seans Saatleri</a><br />
+          <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 10:30</a>
+            </div> 
+             <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 14:30</a>
+            </div> 
+             <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 17:30</a>
+            </div> 
+          </Col>
+        <Col md='3'>
+         </Col>
+      </Row><hr/> <Row>
+        <Col md='3'>
+          <center>
+            <img src='https://img01.imgsinemalar.com/images/afis_buyuk/g/good-morning-veronica-1614678038.jpg' />
+            <br />
+            <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Film Başlık</a>
+          </center>
+          <hr />
+          <div className="butonContainer">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> Bilet Al</a>
+            </div>          
+        </Col>
+        <Col md='6'> 
+        <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Film Başlık</a><br />
+          <p>
+            Bir intihara tanık olduktan sonra buna neden olanları bulmak isteyen alçakgönüllü bir
+            polis memuru, reddedilmiş iki suç dosyasını araştırmaya karar verir. Bu araştırması onu
+            korkunç bir sır saklayan bir çifte yönlendirir.
+          </p><br /> 
+        <a style={{ color: 'black', lineHeight: 2, fontWeight: 'bold' }}>Seans Saatleri</a><br />
+          <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 10:30</a>
+            </div> 
+             <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 14:30</a>
+            </div> 
+             <div className="butonContainer-small">
+          <a style={{ color: 'white', lineHeight: 2, fontWeight: 'bold' }}> 17:30</a>
+            </div> 
+          </Col>
+        <Col md='3'>
+         </Col>
+      </Row><hr/>
     </Container>
   );
 };
