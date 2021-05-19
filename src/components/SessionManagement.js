7d2684f2d56df4fe3e08f2aa00c7e90d92c0a60b
Samet Karakus <karakusnavy@gmail.com>
2021-05-20 00:18:46 +0300

    🔨  main [FIXED] asdsa

--- src/components/SessionManagement.js
+++ src/components/SessionManagement.js
@@ -1,8 +1,8 @@
-import React, { Component, useState, useEffect } from "react";
-import { Container, Col, Form, FormGroup, Label, Input, Row } from "reactstrap";
-import { Link, useParams } from "react-router-dom";
-import firebase from "firebase";
-import firebaseConfig from "../constants/firebase";
+import React, { Component, useState, useEffect } from 'react';
+import { Container, Col, Form, FormGroup, Label, Input, Row } from 'reactstrap';
+import { Link, useParams } from 'react-router-dom';
+import firebase from 'firebase';
+import firebaseConfig from '../constants/firebase';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -15,10 +15,11 @@ const SessionManagement = () => {
   const [id, setId] = useState(null);
   const [showrooms, setShowrooms] = useState([]);
   const [selectedShowroom, setSelectedShowroom] = useState(null);
+  const [selectedFilm, setSelectedFilm] = useState([]);
 
   const { productType } = useParams();
   function clearInputValue() {
-    setTime("");
+    setTime('');
   }
 
   useEffect(() => {
@@ -27,14 +28,14 @@ const SessionManagement = () => {
   }, []);
 
   function GetDataWithId(id) {
-    db.collection(productType)
+    db.collection('films')
       .doc(id)
       .get()
-      .then((querySnapshot) => {
-        // console.log(querySnapshot.id, " => ", querySnapshot.data());
+      .then(querySnapshot => {
+        setSelectedFilm({ id: querySnapshot.id, title: querySnapshot.data().title });
       })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
+      .catch(error => {
+        console.log('Error getting documents: ', error);
       });
     setId(id);
   }
@@ -42,38 +43,82 @@ const SessionManagement = () => {
   const createEmptyPlan = () => {
     var Plan = [];
     for (var i = 0; i < 100; i++) {
-      Plan.push({ id: "0" });
+      Plan.push({ id: '0' });
     }
     return Plan;
   };
 
-  function GetData() {
+  function addSession() {
+    firebase
+      .database()
+      .ref('showrooms/' + selectedShowroom + '/session')
+      .push()
+      .set({
+        hour: time,
+        productId: selectedFilm.id,
+        plan: createEmptyPlan()
+      })
+      .then(() => {
+        alert('eklendi');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+ function GetData() {
     var List = [];
-    db.collection("films")
+    db.collection('films')
       .get()
-      .then((querySnapshot) => {
+      .then(querySnapshot => {
         console.log(querySnapshot);
-        querySnapshot.forEach((doc) => {
+        querySnapshot.forEach(doc => {
           List.push({
             id: doc.id,
             title: doc.data().title,
             description: doc.data().description,
             image: doc.data().image,
             category: doc.data().category,
-            time: doc.data().time,
+            time: doc.data().time
           });
+          console.log('srlam doc');
+          console.log(doc);
         });
+        console.log('---------');
         console.log(List);
-        setItems(List);
+        getSessions(List);
       });
   }
-
+  async function getSessions(filmitem) {
+    console.log(filmitem);
+    console.log('bura gitdi');
+    var newList = [];
+    for (const key in filmitem) {
+      await firebase
+        .database()
+        .ref('showrooms')
+        .once('value', data => {
+          for (const key1 in data.toJSON()) {
+            for (const key2 in data.toJSON()[key1].session) {
+              const sessionData = data.toJSON()[key1].session[key2];
+              if (sessionData.productId == filmitem[key].id) {
+                filmitem[key].time = sessionData.hour;
+                newList.push(filmitem[key]);
+                return
+              }
+            }
+          }
+        });
+    }
+    setItems(newList.reverse());
+    console.log('bura geldi');
+  }
   function getShowRooms() {
     var List = [];
     firebase
       .database()
-      .ref("showrooms")
-      .once("value", (data) => {
+      .ref('showrooms')
+      .once('value', data => {
         for (const key in data.toJSON()) {
           List.push({ id: key, name: data.toJSON()[key].showroomName });
         }
@@ -86,51 +131,48 @@ const SessionManagement = () => {
       <Container
         style={{
           paddingTop: 60,
-          position: "relative",
+          position: 'relative'
         }}
       >
         <Row>
-          <Col md="7">
-            {items.map((item) => (
+          <Col md='7'>
+            {items.map(item => (
               <>
                 <Row>
-                  <Col md="4">
+                  <Col md='4'>
                     <center>
-                      <img
-                        style={{ width: 150, height: 250 }}
-                        src={item.image}
-                      />
+                      <img style={{ width: 150, height: 250 }} src={item.image} />
                       <br />
                       <a
                         style={{
-                          color: "black",
+                          color: 'black',
                           lineHeight: 2,
-                          fontWeight: "bold",
+                          fontWeight: 'bold'
                         }}
                       >
                         {item.title}
                       </a>
                     </center>
                     <hr />
-                    <div className="butonContainer">
+                    <div className='butonContainer'>
                       <a
-                        onClick={() => setId(item.id)}
+                        onClick={() => GetDataWithId(item.id)}
                         style={{
-                          color: "white",
+                          color: 'white',
                           lineHeight: 2,
-                          fontWeight: "bold",
+                          fontWeight: 'bold'
                         }}
                       >
                         Seç
                       </a>
                     </div>
                   </Col>
-                  <Col md="4">
+                  <Col md='4'>
                     <a
                       style={{
-                        color: "black",
+                        color: 'black',
                         lineHeight: 2,
-                        fontWeight: "bold",
+                        fontWeight: 'bold'
                       }}
                     >
                       {item.title}
@@ -140,20 +182,20 @@ const SessionManagement = () => {
                     <br />
                     <a
                       style={{
-                        color: "black",
+                        color: 'black',
                         lineHeight: 2,
-                        fontWeight: "bold",
+                        fontWeight: 'bold'
                       }}
                     >
                       Seans Saatleri
                     </a>
                     <br />
-                    <div className="butonContainer-small">
+                    <div className='butonContainer-small'>
                       <a
                         style={{
-                          color: "white",
+                          color: 'white',
                           lineHeight: 2,
-                          fontWeight: "bold",
+                          fontWeight: 'bold'
                         }}
                       >
                         {item.times}
@@ -165,42 +207,43 @@ const SessionManagement = () => {
               </>
             ))}
           </Col>
-          <Col md="5">
+          <Col md='5'>
             <Row>
-              <Form style={{ alignItems: "center", position: "relative" }}>
+              <Form style={{ alignItems: 'center', position: 'relative' }}>
+                {selectedFilm.title}
                 <FormGroup style={{ width: 450 }}>
                   <Label>Salon Seçiniz</Label>
                   <br />
-                  <select onChange={(e) => setSelectedShowroom(e.target.value)}>
+                  <select onChange={e => setSelectedShowroom(e.target.value)}>
                     <option>Seçiniz...</option>
-                    {showrooms.map((item) => (
+                    {showrooms.map(item => (
                       <option value={item.id}>{item.name}</option>
                     ))}
                   </select>
                   <br />
                   <Label>Seans Saatleri</Label>
                   <Input
-                    onChange={(e) => setTime(e.target.value)}
-                    type="datetime-local"
-                    placeholder="Seans tarihlerini giriniz"
+                    onChange={e => setTime(e.target.value)}
+                    type='datetime-local'
+                    placeholder='Seans tarihlerini giriniz'
                   />
-                  <div className="butonContainer-small">
+                  <div onClick={() => addSession()} className='butonContainer-small'>
                     <a
                       style={{
-                        color: "white",
+                        color: 'white',
                         lineHeight: 2,
-                        fontWeight: "bold",
+                        fontWeight: 'bold'
                       }}
                     >
                       Kaydet
                     </a>
                   </div>
-                  <div className="butonContainer-small">
+                  <div className='butonContainer-small'>
                     <a
                       style={{
-                        color: "white",
+                        color: 'white',
                         lineHeight: 2,
-                        fontWeight: "bold",
+                        fontWeight: 'bold'
                       }}
                     >
                       Güncelle
